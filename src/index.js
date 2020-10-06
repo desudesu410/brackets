27627ad318e8e2dfd3adecc38b724a1298bb5e14
Stanislau Bandarchuk <desudesu410@gmail.com>
2020-10-06 20:47:37 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  bracketsConfig = bracketsConfig.map(element => element.join(''));
+  let bracketsInStr = true;
+  while (bracketsInStr){
+    bracketsInStr = false;
+    for (let i = 0; i < bracketsConfig.length; i++ ) 
+      if (str.includes(bracketsConfig[i])) {
+        str = str.replace(bracketsConfig[i], '')
+        bracketsInStr = true;
+      }  
+  }
+  return str === '' ? true : false;
 }
